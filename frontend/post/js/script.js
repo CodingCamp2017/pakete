cd97e593cb23ab443304fd4051635d3117938ee1
Simon Hessner <uldci@student.kit.edu>
2017-08-21 17:18:33 +0200

    start json

--- frontend/post/js/script.js
+++ frontend/post/js/script.js
@@ -1,12 +1,4 @@
 $(function() {        
-  $("#is_delivered").click(function() {
-    if ($("#is_delivered").prop("checked")) {           
-      $("#station_container").hide()
-    } else {
-      $("#station_container").show()
-    }
-  });
-
   $("#register_link").click(function() {
     $("#update_container").hide();
     $("#update_link_container").removeClass("active");
@@ -19,5 +11,26 @@ $(function() {
     $("#update_link_container").addClass("active");
     $("#register_container").hide();
     $("#register_link_container").removeClass("active");
+  });
+
+  $("#is_delivered").click(function() {
+    if ($("#is_delivered").prop("checked")) {           
+      $("#station_container").hide()
+    } else {
+      $("#station_container").show()
+    }
+  });
+
+  $("#register_packet_button").click(function() {
+    var data = {"sender_street" :   $("#sender_street").val(),
+                "sender_zip" :      $("#sender_zip").val(),
+                "sender_city" :     $("#sender_city").val(),
+                "receiver_street" : $("#receiver_street").val(),
+                "receiver_zip" :    $("#receiver_zip").val(),
+                "receiver_city" :   $("#receiver_city").val(),
+                "weight" :          $("#weight").val()
+                }
+
+    console.log(data);
   });
 });
