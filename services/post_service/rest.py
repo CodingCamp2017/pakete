7eb1972bd2d5154c442c243bc0c0e31bcf9379d9
Antonio Grieco <antonio.grieco@student.uni-augsburg.de>
2017-08-21 17:48:35 +0200

    Added exceptions plus handling.

--- services/post_service/rest.py
+++ services/post_service/rest.py
@@ -1,7 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Module REST
-"""
 
+from flask import Flask, request, abort
+from post_service import register, updateLocation, delivered
+from Exceptions import InvalidActionException, CommandFailedException
 
+app = Flask(__name__)
+
+@app.route('/register', methods=['POST'])
+def restRegister():
+    try:
+    	return register(request.json)
+    except (CommandFailedException, InvalidActionException) as e:
+        return e.message
+
+@app.route('/updateLocation', methods=['POST'])
+def restUpdateLocation():
+    try:
+    	return updateLocation(request.json)
+    except (CommandFailedException, InvalidActionException) as e:
+        return e.message
+    
+@app.route('/delivered', methods=['POST'])
+def restDelivered():
+    try:
+    	return delivered(request.json)
+    except (CommandFailedException, InvalidActionException) as e:
+        return e.message
+    
+if __name__ == '__main__':
+    app.run(debug=True)
