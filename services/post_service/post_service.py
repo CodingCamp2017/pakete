e78069c5fd6ad7e9d23adf69d82fe9a5de0deeb0
ErikP0 <erik.pohle@mnet-mail.de>
2017-08-22 08:52:53 +0100

    Added gitignore & post_service

--- services/post_service/post_service.py
+++ services/post_service/post_service.py
@@ -1,25 +1,49 @@
-from datetime import datetime
+from Exceptions import InvalidActionException, CommandFailedException
 
-def produce_event(package_id, event_type, producer, package=None):
-    message = {}
-    message['id'] = package_id
-    message['type'] = event_type
-    message['time'] = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
-    if package:
-        message['payload'] = package
-    #producer.send('packages', b'hello!')
-    producer.send('packages', message)
+import sys
+import os
+sys.path.append(os.path.relpath('../mykafka'))
 
-def assign_package_id():
-    return 0
+import mykafka
+import re
+import json
 
-def register_package(package, producer):
-    package_id = assign_package_id()
-    produce_event(package_id, 'registered', producer, package)
-    return package_id
 
-def update_package_location(package_id, location, producer):
-    produce_event(package_id, 'updated', producer)
+class PostService:
+    def __init__(self, producer):
+        self.producer = producer
+        
+        
+    def regex_matches_exactly(self, regex, string):
+        match = re.compile(regex).match(string)
+        return match != None and match.end() == len(string)
     
-def mark_delivered(package_id, producer):
-    produce_event(package_id, 'delivered', producer)
+    def checkAvailable(self, dict, req_list):
+        for (key, regex) in req_list:
+            if(not key in dict):
+                raise InvalidActionException("Required key: "+key)
+            elif(not self.regex_matches_exactly(regex, dict[key])):
+                raise InvalidActionException("Invalid value: "+dict[key]+" for key "+key)
+        if(len(dict) == len(req_list)):
+            raise InvalidActionException("Unknown keys")
+    
+    def assign_package_id(self):
+        return 0
+    
+    def register_package(self, jsons):
+        jobj = json.dumps(jsons)
+        self.checkAvailable(jobj, [])
+        package_id = self.assign_package_id()
+        jobj['id'] = package_id
+        mykafka.send(self.producer, 'package', 'registered', 1, jobj)
+        return package_id
+    
+    def update_package_location(self, jsons):
+        jobj = json.dumps(jsons)
+        self.checkAvailable(jobj, [])
+        mykafka.send(self.producer, 'package', 'updated_location', 1, jobj)
+        
+    def mark_delivered(self, jsons):
+        jobj = json.dumbs(jsons)
+        self.checkAvailable(jobj, [])
+        mykafka.send(self.producer, 'delivered', 1, jobj)
