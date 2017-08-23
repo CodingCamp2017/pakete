abf8940a74aba0fdd8100699795dcfb856b6b56f
Josephine Westermann <j.west@mail.de>
2017-08-23 17:43:00 +0200

    updated packet_regex for user-service

--- services/packet_regex/packet_regex.py
+++ services/packet_regex/packet_regex.py
@@ -4,26 +4,49 @@ import json
 
 regex_name = '[\w ]+'
 regex_id = '[\w-]+'
-regex_zip = '\\d{5}'
+regex_zip = '\d{5}'
 regex_street = '[\w( \-\.)?]+'
 regex_city = '[\w( \-)?]+'
 regex_size = '(small|normal|big)'
-regex_weight = '[+-]?(\\d*\\.)?\\d+'
+regex_weight = '[+-]?(\d*\.)?\d+'
 regex_vehicle = '(car|foot|plane|rocket)'
+regex_email = '[\w\d.+]+@[\w\d]+(?:\.[a-z]{2,4}){1,2}'
+regex_password = '\w{8}'
+regex_session_id = '[\w-]+'
 syntax_register = [('sender_name', regex_name),
-                        ('sender_street',regex_street), 
-                        ('sender_zip', regex_zip), 
-                        ('sender_city', regex_city),
-                        ('receiver_name', regex_name),
-                        ('receiver_street', regex_street), 
-                        ('receiver_zip', regex_zip),
-                        ('receiver_city', regex_city), 
-                        ('size', regex_size), 
-                        ('weight', regex_weight)]
+                   ('sender_street',regex_street), 
+                   ('sender_zip', regex_zip), 
+                   ('sender_city', regex_city),
+                   ('receiver_name', regex_name),
+                   ('receiver_street', regex_street), 
+                   ('receiver_zip', regex_zip),
+                   ('receiver_city', regex_city), 
+                   ('size', regex_size), 
+                   ('weight', regex_weight)]
 syntax_update = [('packet_id', regex_id),
-                      ('station', regex_city),
-                      ('vehicle', regex_vehicle)]
+                 ('station', regex_city),
+                 ('vehicle', regex_vehicle)]
 syntax_delivered = [('packet_id', regex_id)]
+syntax_add_user = [('email', regex_email),
+                   ('name',regex_name),
+                   ('street', regex_street),
+                   ('zip', regex_zip), 
+                   ('city', regex_city),
+                   ('password', regex_password)]
+syntax_authenticate_user = [('email', regex_email),
+                            ('password', regex_password)]
+syntax_update_user_adress = [('email', regex_email),
+                             ('street', regex_street),
+                             ('zip', regex_zip), 
+                             ('city', regex_city),
+                             ('session_id', regex_session_id)]
+syntax_add_packet_to_user = [('email', regex_email),
+                             ('packet', regex_id),
+                             ('session_id', regex_session_id)]
+syntax_get_packets_from_user = [('email', regex_email),
+                                ('session_id', regex_session_id)]
+syntax_delete_user = [('email', regex_email),
+                      ('session_id', regex_session_id)]
 
 def regex_matches_exactly(regex, string):
         compRegex = re.compile(regex)
