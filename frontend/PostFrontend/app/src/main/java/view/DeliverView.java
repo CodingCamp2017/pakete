0a43ff0b8f1590ecf7a4eca2abc137295ffea9d2
Antonio Grieco <antonio.grieco@student.uni-augsburg.de>
2017-08-23 10:53:41 +0200

    Added android post frontend project.

---
+++ frontend/PostFrontend/app/src/main/java/view/DeliverView.java
@@ -0,0 +1,14 @@
+package view;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Toni on 23.08.2017.
+ */
+
+public class DeliverView extends View {
+    public DeliverView(Context context) {
+        super(context);
+    }
+}
