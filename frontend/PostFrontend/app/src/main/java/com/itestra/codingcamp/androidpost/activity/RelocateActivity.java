844d1703f96de8a56aef9ceda5c255fce318dac8
Antonio Grieco <antonio.grieco@student.uni-augsburg.de>
2017-08-23 11:51:06 +0200

    Changed navigation to activity base.

---
+++ frontend/PostFrontend/app/src/main/java/com/itestra/codingcamp/androidpost/activity/RelocateActivity.java
@@ -0,0 +1,19 @@
+package com.itestra.codingcamp.androidpost.activity;
+
+import com.itestra.codingcamp.androidpost.R;
+
+/**
+ * Created by Toni on 23.08.2017.
+ */
+
+public class RelocateActivity extends BaseActivity{
+    @Override
+    int getContentViewId() {
+        return R.layout.activity_start;
+    }
+
+    @Override
+    int getNavigationMenuItemId() {
+        return R.id.navigation_relocate;
+    }
+}
