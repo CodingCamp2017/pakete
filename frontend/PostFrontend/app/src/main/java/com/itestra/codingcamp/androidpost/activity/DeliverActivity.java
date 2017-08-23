ff3d559d0e727f5af13a0dbc767188002ca2c0b5
Antonio Grieco <antonio.grieco@student.uni-augsburg.de>
2017-08-23 15:18:58 +0200

    Finished registering.

--- frontend/PostFrontend/app/src/main/java/com/itestra/codingcamp/androidpost/activity/DeliverActivity.java
+++ frontend/PostFrontend/app/src/main/java/com/itestra/codingcamp/androidpost/activity/DeliverActivity.java
@@ -1,5 +1,12 @@
 package com.itestra.codingcamp.androidpost.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
 import com.google.zxing.integration.android.IntentResult;
 import com.itestra.codingcamp.androidpost.R;
 
@@ -19,7 +26,19 @@ public class DeliverActivity extends BaseActivity{
     }
 
     @Override
-    void handleScanResult(IntentResult scanResult) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.floating_action_button_scan);
+        floatingActionButton.setOnClickListener(v -> {
+            if (ActivityCompat.checkSelfPermission(DeliverActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                startScan();
+            } else {
+                String permission = Manifest.permission.CAMERA;
+                if (ContextCompat.checkSelfPermission(DeliverActivity.this, permission) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(DeliverActivity.this, new String[]{permission}, PERMISSION_REQUEST_CODE);
+                }
+            }
+        });
     }
 }
