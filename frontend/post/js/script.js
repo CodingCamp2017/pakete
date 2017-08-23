1493c28c14428a3e6299a8ba9db482a272c4292a
Oliver Brehm <olibrehm@gmail.com>
2017-08-23 18:57:26 +0200

    added page for user registration

--- frontend/post/js/script.js
+++ frontend/post/js/script.js
@@ -27,6 +27,11 @@ $(function() {
       $("#station_container").show()
     }
   });
+  
+  //registration
+  $("#register").click(function() {
+      console.log("register clicked")
+  })
 
   //Send registation
   $("#register_form").submit(function() {
