a6325a07e78181cd22f3d150b856a381f5b70f1d
Josephine Westermann <j.west@mail.de>
2017-08-24 17:43:14 +0200

    neu

--- services/user_service/simulation.py
+++ services/user_service/simulation.py
@@ -8,6 +8,7 @@ import json
 import sys
 import os
 sys.path.append(os.path.relpath('../mykafka'))
+sys.path.append(os.path.relpath('../common'))
 import mykafka
 import uuid
 
@@ -31,12 +32,11 @@ def create_test_add_user_json():
     return json.dumps(data)
 
 def create_simple_test_user():
-    return {'email' : 'oliver3@brehm.de', 'password' : '12345678'}
+    return {'email' : 'olive7r3@brehm.de', 'password' : '12345678'}
 
 
-def create_add_packet_data(email, session_id):
-    return {'email' : email,
-            'packet' : str(uuid.uuid1()),
+def create_add_packet_data(session_id):
+    return {'packet' : str(uuid.uuid1()),
             'session_id' : session_id}
         
 def create_session_data(email, session_id):
@@ -45,7 +45,7 @@ def create_session_data(email, session_id):
 
 
 def simulate_user_behaviour():
-    user_service = UserService(mykafka.create_producer('ec2-35-159-21-220.eu-central-1.compute.amazonaws.com', 9092))
+    user_service = UserService()
     for i in range(1):
         user_data = create_simple_test_user()
         email = user_data['email']
@@ -60,13 +60,13 @@ def simulate_user_behaviour():
             #time.sleep(randint(1,2))
             for j in range(randint(0,10)):
                 if randint(0,1):
-                    user_service.add_packet_to_user(create_add_packet_data(email, session_id))
+                    user_service.add_packet_to_user(create_add_packet_data(session_id))
                     print('User {} added packet'.format(user_data['email']))
                 else:
-                    packets = user_service.get_packets_from_user(create_session_data(email, session_id))
+                    packets = user_service.get_packets_from_user(session_id)
                     print('User {} has packets'.format(user_data['email'], packets))
                 #time.sleep(randint(1,2))
-            user_service.logout_user(create_session_data(user_data['email'], session_id))
+            user_service.logout_user(session_id)
             print('User {} logged out'.format(user_data['email']))
             #time.sleep(randint(1,2))
     
