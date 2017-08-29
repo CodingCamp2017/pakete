4757f8cfeaa06078e335935fbc65b2e8cef62c5e
Antonio Grieco <antonio.grieco@student.uni-augsburg.de>
2017-08-29 16:22:15 +0200

    Added new functionality to alexa. NOT RUNNABLE.

--- frontend/alexa/alexa_flask_ask.py
+++ frontend/alexa/alexa_flask_ask.py
@@ -13,17 +13,42 @@ packets = [
 '5ee70a06-11d5-4198-b8d0-7812a1aa21b2'
 ]
 
-BASE_URL = 'http://ec2-35-158-239-16.eu-central-1.compute.amazonaws.com:8001'
-
+TRACKING_BASE_URL = 'http://ec2-35-158-239-16.eu-central-1.compute.amazonaws.com:8001'
+USER_BASE_URL = 'http://ec2-35-158-239-16.eu-central-1.compute.amazonaws.com:8002'
 
 app = Flask(__name__)
 ask = Ask(app, '/')
+
+class AlexaPacketTrackingData:
+    def __init__(self, email='', password=''):
+        self.email = email
+        self.password = password
+        self.sessionId = ''
+        self.packetIds = list()
+        self.packetsVerbose = list()
+
+    def setSessionId(sessionId):
+        self.sessionId = sessionId
+
+    def setPacketIds(packetIds):
+        self.packetIds = packetIds
+
+    def addVerbosePacket(id, packet):
+        self.packetsVerbose.append(packet)
+
+
+
+authData = AlexaPacketTrackingData(email='test@test.de', password='testtest')
+
+#########
+# Intents
+#########
 
 @ask.launch
 def launch():
     welcome_text = render_template('welcome')
     help_text = render_template('help')
-    return question(welcome_text).reprompt(help_text)
+    return question(welcome_text)
 
 @ask.intent('AMAZON.HelpIntent')
 def help():
@@ -41,38 +66,91 @@ def cancel():
     stop_text = render_template('stop')
     return statement(stop_text)
 
-@ask.intent('StartTrackingIntent')
-def start_track():
-    get_packet_number_text = render_template('get_packet_number')
-    return question(get_packet_number_text)
-
-@ask.intent('TrackingIntent', convert={'packet_number': int})
-def track(packet_number):
-    packet_info = get_packet_info(packet_number)
-    if not packet_info:
-        error_text = render_template('error')
-        help_text = render_template('help')
-        return question(error_text).reprompt(help_text)
-    packet_info = json.loads(packet_info)
-    if 'deliveryTime' in packet_info:
-        already_delivered_text = render_template('already_delivered')
-        return question(already_delivered_text)
-    elif not packet_info['stations']:
-        just_sent_text = render_template('just_sent')
-        return question(just_sent_text)
+@ask.intent('LoginIntent')
+def login():
+    authenticate()
+    if authData.sessionId:
+        login_success_text = render_template('login_success')
+        return question(login_success_text)
     else:
-        current_city = packet_info['stations'][-1]['location']
-        packet_located_at_text = render_template('packet_located_at', city=current_city)
-        return question(packet_located_at_text)
+        error_text = render_template('error')
+        return statement(error_text)
 
-def get_packet_info(packet_number):
-    if packet_number > len(packets):
-        return None
-    packet_number = packets[packet_number-1]
-    request = urllib.request.Request(BASE_URL + '/packetStatus/' + packet_number)
-    response = urllib.request.urlopen(request)
-    responseJson = json.loads(response.read().decode('utf8'))
-    return responseJson
+@ask.intent('PaketNumberIntent')
+def packet_number():
+    fetchPacketIds()
+    number_of_packets_text = render_template('number_of_packets', number=len(packetIds))
+    return question(number_of_packets_text)
+
+@ask.intent('OverviewIntent')
+def overview():
+    fetchPacketData()
+    overviewDict = dict()
+    for packetInfo in authData.packetsVerbose:
+        if not (packetInfo['sender_name'], packetInfo['receiver_name']) in overviewDict:
+            overviewDict[(packetInfo['sender_name'], packetInfo['receiver_name'])] = [packetInfo]
+        else:
+            overviewDict[(packetInfo['sender_name'], packetInfo['receiver_name'])].append(packetInfo)
+
+    overviewMessages = list()
+    for senderReceiverTuple in overviewDict:
+        overviewMessages.append(render_template('there_are_x_packets', 
+            number=len(overviewDict[senderReceiverTuple])),
+            sender=senderReceiverTuple[0],
+            receiver=senderReceiverTuple[1])
+
+    return question('...'.join(overviewMessages))
+
+########
+# Filter
+########
+
+@app.template_filter()
+def speakable_number(number):
+    if number == 1:
+        return render_template('speakable_one')
+    return number
+
+@app.template_filter()
+def plural_e(number):
+    if number != 1:
+        return 'e'
+    return ''
+
+@app.template_filter()
+def plural_n(number):
+    if number != 1:
+        return 'n'
+    return ''
+
+
+#########
+# Helper
+#########
+
+def fetchPacketData():
+    for packetId in authData.packetIds:
+        request = urllib.request.Request(TRACKING_BASE_URL + '/packetStatus/' + packet_number)
+        response = urllib.request.urlopen(request)
+        responseJson = json.loads(json.loads(response.read().decode('utf8')))
+        authData.addVerbosePacket(packetId, responseJson)
+
+# def get_packet_info(packet_number):
+#     if packet_number > len(packets):
+#         return None
+#     packet_number = packets[packet_number-1]
+#     request = urllib.request.Request(TRACKING_BASE_URL + '/packetStatus/' + packet_number)
+#     response = urllib.request.urlopen(request)
+#     responseJson = json.loads(response.read().decode('utf8'))
+#     return responseJson
+
+def fetchPacketIds():
+    authData.setPacketIds(packets)
+
+def authenticate():
+    pass
+    #createNewUser()
+    session = None# session
 
 if __name__ == '__main__':
     app.run()
