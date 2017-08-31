e776f13abd92f368a04ed27ea8f29f693e4e40b9
ErikP0 <erik.pohle@mnet-mail.de>
2017-08-31 15:20:55 +0100

    Added sorting at client side

--- frontend/data_warehouse/js/statistics.js
+++ frontend/data_warehouse/js/statistics.js
@@ -17,15 +17,40 @@
       tooltips: { enabled: false }
     });
     */
-  });  
+  });
+
+  let size_compare_weight = ['small', 'normal', 'big']
+
+  let alphabetic_order = function (a, b) {
+    return (a.key).localeCompare(b.key)
+  }
+
+  let hour_order = function (a, b) {
+    return Number(a.key)-Number(b.key)
+  }
+
+  let day_order = function (a, b) {
+    //dd.mm.yyyy
+    let splita = a.key.split('.')
+    let splitb = b.key.split('.')
+    let dateA = new Date(splita[1]+"-"+splita[0]+"-"+splita[2])
+    let dateB = new Date(splitb[1]+"-"+splitb[0]+"-"+splitb[2])
+    return (dateA > dateB) - (dateA < dateB)
+  }
+
+  let ascending_values_numeric = function (a, b) {
+    return a.value - b.value
+  }
+
+
 
   app.controller('ChartController', ['$scope', '$http', '$attrs', '$element', function ($scope, $http, $attrs, $element) {
     $scope.labels = [];
     $scope.series = ['Pakete'];
-    $scope.data   = [[]];    
+    $scope.data   = [[]];
     $scope.type_options = ["line", "pie", "bar"];
     $scope.type     = $scope.type_options[0];
-    $scope.options  = { legend: { display: true } };    
+    $scope.options  = { legend: { display: true } };
     $scope.all_time = true
 
     $scope.dataLoaded = false
@@ -39,29 +64,37 @@
     $scope.no_data_available = false
 
     $scope.information_options = {
-        "sizes"         : {"url" : "size", "type" : 1},
-        "vehicles"      : {"url" : "location_vehicle_current", "type" : 1},
-        "avg_delivery"  : {"url" : "average_delivery/day", "type" : 2},
-        "location_day"  : {"url" : "location_vehicle_current/day", "type" : 3},
-        "location_hour" : {"url" : "location_vehicle_current/hour", "type" : 3},
-        "avg_weight_day":{"url" : "average_weight/day", "type" : 2},
-        "registrations_day":{"url" : "registration/day", "type" : 2},
-        "registrations_hour":{"url" : "registration/hour", "type" : 2},
+        "sizes"         : {"url" : "size", "type" : 1,
+        "sortfun" : function (a, b) {
+            let index_a = size_compare_weight.indexOf(a.key)
+            let index_b = size_compare_weight.indexOf(b.key)
+            return index_a-index_b
+        }, },
+        "vehicles"      : {"url" : "location_vehicle_current", "type" : 1, "sortfun" : alphabetic_order,
+        "filterfun" : function (text, zahl) {
+            return text != "registert" && text != "delivery"
+        }},
+        "avg_delivery"  : {"url" : "average_delivery/day", "type" : 2, "sortfun" : day_order, 'unit' : 'Lieferzeit in Sekunden'},
+        "location_day"  : {"url" : "location_vehicle_current/day", "type" : 3, "sortfun" : day_order},
+        "location_hour" : {"url" : "location_vehicle_current/hour", "type" : 3, "sortfun" : hour_order},
+        "avg_weight_day":{"url" : "average_weight/day", "type" : 2, "sortfun" : day_order, 'unit' : 'Gewicht in kg'},
+        "registrations_day":{"url" : "registration/day", "type" : 2, "sortfun" : day_order},
+        "registrations_hour":{"url" : "registration/hour", "type" : 2, "sortfun" : hour_order},
         "addresses"     : {"url" : "location_address_current", "type" : 1,
         "filterfun": function (text, zahl) {
             return text != "registert" && text != "delivery" && zahl >= 5
-        }},
+        }, "sortfun" : ascending_values_numeric},
         "city_send"     : {"url" : "sender_city", "type" : 1,
         "filterfun" : function (text, zahl) {
             return zahl > 100
-        }},
+        }, "sortfun" : ascending_values_numeric},
         "city_receive"     : {"url" : "receiver_city", "type" : 1,
         "filterfun" : function (text, zahl) {
             return zahl > 100
-        }}
+        }, "sortfun" : ascending_values_numeric}
     }
 
-    
+
     $scope.$watch('from_date', function (value) {
       try {
        $scope.from_date_obj = new Date(value);
@@ -89,14 +122,14 @@
 
 
     $scope.openFromCalendar = function() {
-      $element.find("#from_picker input").trigger("click")      
+      $element.find("#from_picker input").trigger("click")
     }
 
     $scope.openToCalendar = function() {
-      $element.find("#to_picker input").trigger("click")      
+      $element.find("#to_picker input").trigger("click")
     }
 
-    $scope.setData = function() {      
+    $scope.setData = function() {
       $scope.dataLoaded = false
 
       var time_filter_url_string = ""
@@ -106,7 +139,7 @@
         var to_unix = Math.floor($scope.to_date_obj.getTime()/1000)
         time_filter_url_string = from_unix + "/" + to_unix + "/"
       }
-      
+
 
       var information = $scope.information_options[$attrs.information]
       var url = "http://localhost:8000/" + time_filter_url_string + information["url"]
@@ -114,68 +147,122 @@
 
       $scope.series = $scope.series.slice(0,0)
       if (result_type != 3) {
-        $scope.series.push("Pakete")
+        let unit = information.unit ? information.unit : "Pakete"
+        $scope.series.push(unit)
       }
 
-      $http.get(url).then(function success(response) {      
+      $http.get(url).then(function success(response) {
         console.log($element)
-        
+
+        //transform the data into array of key, value pairs
+        var statistic_data = []
+        for (var datakey in response.data.values) {
+          statistic_data.push({key:datakey, value:response.data.values[datakey]})
+        }
+
+        //if available, sort the data
+        if (information.sortfun) {
+          statistic_data.sort(information.sortfun)
+        }
+
         $scope.data = $scope.data.slice(0,0)
         $scope.labels = $scope.labels.slice(0,0)
-        
+
         if (result_type == 3) {
-          for (var key in response.data.values) {
-            if (response.data.values.hasOwnProperty(key)) {
-              // Add all keynames in response (car, train, ...) as own series
-              for (var keyname in Object.keys(response.data.values[key])) {
-                if ($scope.series.indexOf(Object.keys(response.data.values[key])[keyname]) == -1) {
-                  $scope.series.push(Object.keys(response.data.values[key])[keyname])                          
-                }                
+          for(var i=0; i< statistic_data.length; ++i) {
+            let datapoint = statistic_data[i]
+            for(var value_index in Object.keys(datapoint.value)) {
+              let subkey = Object.keys(datapoint.value)[value_index]
+              if($scope.series.indexOf(subkey) == -1 && subkey != "registert") {
+                $scope.series.push(subkey)
               }
             }
           }
-
-          for (var i = 0; i < $scope.series.length; ++i) {
-            var new_data_arr = []            
-            for (var j = 0; j < Object.keys(response.data.values).length; ++j) {
-              new_data_arr.push(0)
-            }
+          for (var i = 0; i< $scope.series.length; ++i) {
+            var new_data_arr = new Array(statistic_data.length)
+            new_data_arr.fill(0)
             $scope.data.push(new_data_arr)
           }
+
+
+          // for (var key in response.data.values) {
+          //   if (response.data.values.hasOwnProperty(key)) {
+          //     // Add all keynames in response (car, train, ...) as own series
+          //     for (var keyname in Object.keys(response.data.values[key])) {
+          //       if ($scope.series.indexOf(Object.keys(response.data.values[key])[keyname]) == -1) {
+          //         $scope.series.push(Object.keys(response.data.values[key])[keyname])
+          //       }
+          //     }
+          //   }
+          // }
+
+          // for (var i = 0; i < $scope.series.length; ++i) {
+          //   var new_data_arr = []
+          //   for (var j = 0; j < Object.keys(response.data.values).length; ++j) {
+          //     new_data_arr.push(0)
+          //   }
+          //   $scope.data.push(new_data_arr)
+          // }
         } else {
           // Type 1, 2
           $scope.data.push([])
         }
 
         var didx = 0
-        for (var key in response.data.values) {
-          if (response.data.values.hasOwnProperty(key)) {            
-            if (result_type == 3) {
-              // Add all keynames in response (car, train, ...) as own series
-              for (var keyname in Object.keys(response.data.values[key])) {
-                var keyname_str = Object.keys(response.data.values[key])[keyname]
-                var series_idx = $scope.series.indexOf(keyname_str)                
-                $scope.data[series_idx][didx] = response.data.values[key][keyname_str]
-                
-                if ($scope.labels.indexOf(key) == -1) {
-                  $scope.labels.push(key)
+        for (var i = 0; i<statistic_data.length; ++i) {
+          let datapoint = statistic_data[i]
+          if(result_type == 3) {
+            // Add all keynames in response (car, train, ...) as own series
+            let keys = Object.keys(datapoint.value)
+            for (var subkey in keys) {
+              var subkey_str = keys[subkey]
+              var series_idx = $scope.series.indexOf(subkey_str)
+              if (series_idx >= 0) {
+                $scope.data[series_idx][didx] = datapoint.value[subkey_str]
+                if($scope.labels.indexOf(datapoint.key) == -1) {
+                  $scope.labels.push(datapoint.key)
                 }
               }
-            } else {
-              // Type 1, 2
-              let value = response.data.values[key]
-              if (!information.filterfun || information.filterfun(key, value)) {
-                $scope.data[0].push(value)
-                $scope.labels.push(key)
-              }
+            }
+          }else{
+            //Type 1,2
+            if(!information.filterfun || information.filterfun(datapoint.key, datapoint.value)) {
+              $scope.data[0].push(datapoint.value)
+              $scope.labels.push(datapoint.key)
             }
           }
           didx++
         }
 
+
+        // for (var key in response.data.values) {
+        //   if (response.data.values.hasOwnProperty(key)) {
+        //     if (result_type == 3) {
+        //       // Add all keynames in response (car, train, ...) as own series
+        //       for (var keyname in Object.keys(response.data.values[key])) {
+        //         var keyname_str = Object.keys(response.data.values[key])[keyname]
+        //         var series_idx = $scope.series.indexOf(keyname_str)
+        //         $scope.data[series_idx][didx] = response.data.values[key][keyname_str]
+        //
+        //         if ($scope.labels.indexOf(key) == -1) {
+        //           $scope.labels.push(key)
+        //         }
+        //       }
+        //     } else {
+        //       // Type 1, 2
+        //       let value = response.data.values[key]
+        //       if (!information.filterfun || information.filterfun(key, value)) {
+        //         $scope.data[0].push(value)
+        //         $scope.labels.push(key)
+        //       }
+        //     }
+        //   }
+        //   didx++
+        // }
+
         $scope.dataLoaded = true
-      }, function error() { 
-        console.log("error");        
+      }, function error() {
+        console.log("error");
       })
     }
 
@@ -206,7 +293,7 @@
 
   app.component("statistic", {
     "templateUrl": "templates/statistic.html",
-    "bindings": {"name" : "@", "type" : "@"},    
+    "bindings": {"name" : "@", "type" : "@"},
     "controller": 'ChartController'
   });
 
@@ -215,28 +302,28 @@
   app.controller('PieCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.labels = [];
     $scope.data = [];
-    $scope.options = { legend: { display: true } };    
+    $scope.options = { legend: { display: true } };
 
     $scope.dataLoaded = false;
 
     $scope.setData = function() {
       $scope.dataLoaded = false
 
-      $http.get("http://localhost:8000/getSizeDistribution").then(function success(response) {                   
-        console.log(response.data)      
+      $http.get("http://localhost:8000/getSizeDistribution").then(function success(response) {
+        console.log(response.data)
         $scope.data = $scope.data.slice(0,0)
         $scope.labels = $scope.labels.slice(0,0)
 
         for (var key in response.data.values) {
-          if (response.data.values.hasOwnProperty(key)) {            
-            $scope.data.push(response.data.values[key])          
+          if (response.data.values.hasOwnProperty(key)) {
+            $scope.data.push(response.data.values[key])
             $scope.labels.push(key)
           }
         }
 
         $scope.dataLoaded = true
-      }, function error() { 
-        alert("error");        
+      }, function error() {
+        alert("error");
       })
     }
 
@@ -244,7 +331,7 @@
 
   }]);
 
-  
+
   app.controller('MenuCtrl', ['$scope', function ($scope) {
     $scope.isCollapsed = true;
     $scope.charts = ['Line', 'Bar', 'Doughnut', 'Pie', 'Polar Area', 'Radar', 'Horizontal Bar', 'Bubble', 'Base'];
@@ -269,7 +356,7 @@
     }, 500);
   }]);
 
-  
+
 
   app.controller('PolarAreaCtrl', ['$scope', function ($scope) {
     $scope.labels = ['Download Sales', 'In-Store Sales', 'Mail Sales', 'Telesales', 'Corporate Sales'];
